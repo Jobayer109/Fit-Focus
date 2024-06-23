5cd376cca78cf8f3458222a76758f1e59c368cdf
user.email <jobayer.ahmed109@gmail.com>
2024-06-23 23:43:23 +0600

    Banner responsiveness done

--- client/src/Components/Banner/Banner.jsx
+++ client/src/Components/Banner/Banner.jsx
@@ -1,96 +1,92 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import '../../Styles/SwiperSlide.css';
-import 'animate.css';
-import { Navigation, Autoplay, Pagination } from 'swiper/modules';
-import { Helmet } from 'react-helmet';
-import bg from "../../../public/bg1.gif";
-import bg1 from "../../../public/bg2.gif";
-import { Link } from 'react-router-dom';
+import { Helmet } from "react-helmet";
+import { Link } from "react-router-dom";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import bg1 from "../../../public/bg1.gif";
+import bg2 from "../../../public/bg2.gif";
+import "../../Styles/SwiperSlide.css";
 
 const Home = () => {
   return (
-    <div className="container mx-auto ">
+    <div className="bg-gray-900">
       <Helmet>
-        <title>FITFOCUS | Home</title>
+        <title>FitFocus | Home</title>
       </Helmet>
-      <div className='relative font-Poppins'>
-        <Swiper
-          spaceBetween={0}
-          centeredSlides={true}
-          autoplay={{
-            delay: 5500,
-            disableOnInteraction: false,
-          }}
-          pagination={{
-            clickable: true,
-          }}
-          navigation={true}
-          modules={[Autoplay, Pagination, Navigation]}
-          className="mySwiper"
-        >
-          <SwiperSlide className='bg-gradient-to-r  rounded-xl from-slate-900 to-black  '>
-            <div
-              className="bg-cover rounded-xl mr-0 bg-center hero w-full h-[580px] bg-gradient-to-r from-slate-900 to-black"
-              style={{
-                backgroundImage: `url(${bg1})`,
-              }}
-            >
-              <div className='w-full h-full bg-opacity-5 flex justify-center items-center bg-slate-100'>
-                <div className="hero-content  text-center text-neutral-content animate__animated animate__fadeInUp animate__slower">
-                  <div className="lg:max-w-lg md:max-w-md w-[100%]  lg:w-[100%]">
-                    <h1 className="mb- text-xl lg:text-3xl font-bold text-[#000] p-3 bg-opacity-5  font-Prata">
-                      FITFOCUS: Shape Your Future
+      <div className="container mx-auto max-w-screen-xl">
+        <div className="relative font-Poppins">
+          <Swiper
+            spaceBetween={0}
+            centeredSlides={true}
+            autoplay={{
+              delay: 5500,
+              disableOnInteraction: false,
+            }}
+            pagination={{
+              clickable: true,
+            }}
+            navigation={true}
+            modules={[Autoplay, Pagination, Navigation]}
+            className="mySwiper"
+          >
+            <SwiperSlide className="bg-gradient-to-r from-slate-900 to-black">
+              <div
+                className="bg-cover bg-center bg-no-repeat h-[580px] relative"
+                style={{
+                  backgroundImage: `url(${bg1})`,
+                }}
+              >
+                <div className="absolute inset-0 bg-black opacity-50"></div>
+                <div className="absolute inset-0 flex justify-center items-center text-center">
+                  <div className="text-white text-center">
+                    <h1 className="text-4xl lg:text-6xl font-bold mb-4 text-gray-200">
+                      Shape Your Future with{" "}
+                      <span className="text-orange-600">FitFocus</span>
                     </h1>
-                    <p className="mb-5 text-[#fff] font-semibold">
-                      Welcome to <a className="ml-2 text-2xl font-bold italic text-[#1882FF]">FITFOCUS .</a>
-                      Your Ultimate Destination for Health, Wellness, and Fitness. Empowering You to Train, Transform, and Achieve Peak Performance Every Day.
+                    <p className="text-lg lg:text-xl md:text-md sm:text-sm mb-6  text-gray-300">
+                      Your Ultimate Destination for Health, Wellness, and
+                      Fitness
                     </p>
                     <Link to="/trainers">
-                      <button
-                        className="align-middle mr-3 select-none font-Rilway text-lg font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none  py-3 px-6 rounded-lg bg-[#1E1743] border m-5  text-[#fff] hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85]"
-                        type="button"
-                      >
-                        See Our Trainer→
+                      <button className="px-6 py-3 bg-orange-600 text-white font-semibold uppercase rounded-lg hover:opacity-85 focus:ring focus:ring-gray-300 inline-block">
+                        See Our Trainers →
                       </button>
                     </Link>
                   </div>
                 </div>
               </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className='bg-gradient-to-r from-slate-900 to-black '>
-            <div
-              className="bg-cover mr-0 bg-center hero w-full h-[580px] bg-gradient-to-r from-slate-900 to-black"
-              style={{
-                backgroundImage: `url(${bg})`,
-              }}
-            >
-              <div className="hero-content text-center text-neutral-content animate__animated animate__fadeInUp animate__slower">
-                <div className="lg:max-w-lg md:max-w-md w-[100%]  lg:w-[100%]">
-                  <h1 className="mb- text-xl lg:text-3xl font-bold text-[#fff] p-3 bg-opacity-5  font-Prata">
-                    FITFOCUS: Shape Your Future
-                  </h1>
-                  <p className="mb-5 text-[#fff] font-semibold">
-                    Welcome to <a className="ml-2 text-2xl font-bold italic text-[#1E1743]">FITFOCUS .</a>
-                    Your Ultimate Destination for Health, Wellness, and Fitness. Empowering You to Train, Transform, and Achieve Peak Performance Every Day.
-                  </p>
-                  <Link to="/trainers">
-                    <button
-                      className="align-middle mr-3 select-none font-Rilway text-lg font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none  py-3 px-6 rounded-lg bg-[#1E1743] border m-5 text-[#fff] hover:opacity-75 focus:ring focus:ring-gray-300 active:opacity-[0.85]"
-                      type="button"
-                    >
-                      See Our Trainer→
-                    </button>
-                  </Link>
+            </SwiperSlide>
+            <SwiperSlide className="bg-gradient-to-r from-slate-900 to-black">
+              <div
+                className="bg-cover bg-center h-[580px] relative"
+                style={{
+                  backgroundImage: `url(${bg2})`,
+                }}
+              >
+                <div className="absolute inset-0 bg-black opacity-50"></div>
+                <div className="absolute inset-0 flex justify-center items-center text-center">
+                  <div className="text-white text-center">
+                    <h1 className="text-4xl lg:text-6xl font-bold mb-4 text-gray-200">
+                      Shape Your Future with{" "}
+                      <span className="text-orange-600">FitFocus</span>
+                    </h1>
+                    <p className="text-lg lg:text-xl mb-6 text-gray-300">
+                      Your Ultimate Destination for Health, Wellness, and
+                      Fitness
+                    </p>
+                    <Link to="/trainers">
+                      <button className="px-6 py-3 bg-orange-600 text-white font-semibold uppercase rounded-lg hover:opacity-85 focus:ring focus:ring-gray-300 inline-block">
+                        See Our Trainers →
+                      </button>
+                    </Link>
+                  </div>
                 </div>
               </div>
-            </div>
-          </SwiperSlide>
-        </Swiper>
+            </SwiperSlide>
+          </Swiper>
+        </div>
       </div>
-
     </div>
   );
 };
