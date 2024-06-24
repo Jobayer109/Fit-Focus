fe18de64cfae9d4e961fb03ad79e91125cce847f
user.email <jobayer.ahmed109@gmail.com>
2024-06-24 07:48:28 +0600

    Main loading page centered

--- client/src/Layout/Root/Root.jsx
+++ client/src/Layout/Root/Root.jsx
@@ -1,32 +1,30 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../../Components/Footer/Footer";
 import Header from "../../Components/Header/Header";
 import UseAuth from "../../Hook/useAuth";
-import Footer from "../../Components/Footer/Footer";
-
 
 const Root = () => {
-    const { loading } = UseAuth();
-    if (loading) {
-// set loading
-        return <div className="bg-[#1E1743] h-screen flex justify-center items-center">
-            <div>
-                <span className=" text-white m-auto loading loading-spinner w-20"></span>
-                <div className="flex items-center justify-center  -ml-[30%]">
-
-                    <h1 className="mt-5 mr-10 tracking-widest text-[#FFFFFF] font-Prata font text-5xl">FITFOCUS</h1>
-                </div>
-            </div>
-
-
-        </div>
-    }
+  const { loading } = UseAuth();
+  if (loading) {
+    // set loading
     return (
-        <div className="w-full" >
-            <Header className="w-full "></Header>
-            <Outlet></Outlet>
-            <Footer></Footer>
+      <div className="bg-[#1E1743] h-screen flex justify-center items-center">
+        <div className="flex flex-col items-center">
+          <span className="text-white m-auto loading loading-spinner w-20"></span>
+          <h1 className="mt-5 tracking-widest text-orange-600 font-Prata font-extrabold text-5xl">
+            FITFOCUS
+          </h1>
         </div>
+      </div>
     );
+  }
+  return (
+    <div className="w-full">
+      <Header className="w-full "></Header>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Root;
