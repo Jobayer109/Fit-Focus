76eda0486b2d8677145b82f654306ffcb3dc58ad
user.email <jobayer.ahmed109@gmail.com>
2024-06-25 22:09:18 +0600

    Categories section initial completation done

--- client/src/Components/FeaturedClasses/FeaturedClasse.jsx
+++ client/src/Components/FeaturedClasses/FeaturedClasse.jsx
@@ -1,45 +1,49 @@
-import { useState } from "react";
-import { MdPreview } from "react-icons/md";
-
+/* eslint-disable react/prop-types */
+// import { useState } from "react";
+import { GiBullseye } from "react-icons/gi";
 const FeaturedClasse = ({ classData }) => {
-  const [showFullDetails, setShowFullDetails] = useState(false);
+  // const [showFullDetails, setShowFullDetails] = useState(false);
 
-  const toggleDetails = () => {
-    setShowFullDetails(!showFullDetails);
-  };
+  // const toggleDetails = () => {
+  //   setShowFullDetails(!showFullDetails);
+  // };
 
   return (
     <div
-      className="flex justify-evenly font-Rilway item-center rounded-md shadow-md bg-slate-100 dark:bg-gray-50 dark:text-gray-800 border border-[#e6e5eb]"
+      className="flex justify-between h-60 item-center rounded-md shadow-md bg-slate-900  border border-orange-400"
       data-aos="zoom-in"
     >
       <img
-        width={150}
-        height={200}
         src={classData.image}
         alt=""
-        className="rounded-md dark:bg-gray-500"
+        className="rounded-md h-full w-[30%] dark:bg-gray-500"
       />
       <div className="flex flex-col justify-between p-6 space-y-8">
         <div className="space-y-2">
-          <h2 className="text-3xl font-semibold tracking-wide">
+          <h2 className="text-3xl text-gray-300 font-semibold tracking-">
             {classData.className}
           </h2>
-          <p className="dark:text-gray-800 text-sm">
+          <div className="flex items-center justify-start font-bold ">
+            <GiBullseye className="text-orange-500" size={18} />
+            <p className="text-slate-400 ml-1">
+              Total book:{" "}
+              <span className="text-orange-400">{classData.totalBook}</span>
+            </p>
+          </div>
+          <p className="text-sm text-gray-400 leading-tight">
+            {classData.details}
+          </p>
+          {/* <p className="dark:text-gray-800 text-sm text-gray-400 leading-tight">
             {showFullDetails
               ? classData.details
               : classData.details.slice(0, 100)}
             <span
-              className="text-[#1882FF] font-bold text-lg ml-2 cursor-pointer"
+              className="text-[#1882FF]  text-sm ml-2 cursor-pointer "
               onClick={toggleDetails}
             >
               {showFullDetails ? "see less" : "see more"}
             </span>
-          </p>
-        </div>
-        <div className="flex item-center font-bold ">
-          <MdPreview size={25} />
-          <p>Total book: {classData.totalBook}</p>
+          </p> */}
         </div>
       </div>
     </div>
