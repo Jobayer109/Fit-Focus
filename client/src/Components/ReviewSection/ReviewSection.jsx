7328f299033de5a9f3b0dd9ce30ffee24b9fc7f8
user.email <jobayer.ahmed109@gmail.com>
2024-06-26 19:57:43 +0600

    SOme important issues are fixed

--- client/src/Components/ReviewSection/ReviewSection.jsx
+++ client/src/Components/ReviewSection/ReviewSection.jsx
@@ -22,12 +22,12 @@ const ReviewSection = () => {
   });
 
   return (
-    <div className="container mx-auto w-[90%] mb-16 mt-36">
+    <div className="container mx-auto w-[90%] mt-36">
       <div className="text-center">
-        <h1 className="text-4xl font-Prata font-bold text-orange-500">
+        <h1 className="text-4xl font-Prata font-bold  text-orange-500">
           What Our Clients Say
         </h1>
-        <div className="h-2 w-96 mt-1  bg-orange-300 mx-auto blur-lg"></div>
+        <div className="h-2 w-96 mt-1 mb-10 bg-orange-300 mx-auto blur-lg"></div>
       </div>
       <Swiper
         slidesPerView={1}
@@ -64,14 +64,14 @@ const ReviewSection = () => {
       >
         {data?.map((review) => (
           <SwiperSlide key={review._id}>
-            <div className="container flex flex-col w-full h-72 max-w-lg p-6 mx-auto divide-y rounded-md text-white border-2 border-orange-500 mt-12">
-              <div className="flex justify-between p-4">
-                <div className="flex space-x-4">
+            <div className="container flex flex-col w-full h-72 max-w-lg p-6 mx-auto divide-y rounded-md text-white border-2 border-gray-500 relative z-10 bg-gray-800">
+              <div className="flex items-center justify-between pb-2">
+                <div className="flex items-center  space-x-2">
                   <div>
                     <img
                       src={review.image}
                       alt={review.name}
-                      className="object-cover w-12 h-12 rounded-full border-2"
+                      className="object-cover w-12 h-12 rounded-full border-2 border-orange-500"
                     />
                   </div>
                   <div className="leading-tight">
@@ -83,15 +83,22 @@ const ReviewSection = () => {
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 512 512"
-                    className="w-5 h-5 fill-current"
+                    className="w-5 h-5 fill-orange-500"
                   >
                     <path d="M494,198.671a40.536,40.536,0,0,0-32.174-27.592L345.917,152.242,292.185,47.828a40.7,40.7,0,0,0-72.37,0L166.083,152.242,50.176,171.079a40.7,40.7,0,0,0-22.364,68.827l82.7,83.368-17.9,116.055a40.672,40.672,0,0,0,58.548,42.538L256,428.977l104.843,52.89a40.69,40.69,0,0,0,58.548-42.538l-17.9-116.055,82.7-83.368A40.538,40.538,0,0,0,494,198.671Zm-32.53,18.7L367.4,312.2l20.364,132.01a8.671,8.671,0,0,1-12.509,9.088L256,393.136,136.744,453.3a8.671,8.671,0,0,1-12.509-9.088L144.6,312.2,50.531,217.37a8.7,8.7,0,0,1,4.778-14.706L187.15,181.238,248.269,62.471a8.694,8.694,0,0,1,15.462,0L324.85,181.238l131.841,21.426A8.7,8.7,0,0,1,461.469,217.37Z"></path>
                   </svg>
-                  <span className="text-xl font-bold">{review.rating}</span>
+                  <span className="text-xl font-bold text-gray-200">
+                    {review.rating}
+                  </span>
                 </div>
               </div>
-              <div className="p-4 space-y-2 text-sm text-gray-300">
-                {review.review}
+              <div className="p-4 space-y-2 text-sm text-gray-300  ">
+                <img
+                  src="https://www.svgrepo.com/show/479031/quote.svg"
+                  alt=""
+                  className="w-24 h-24 absolute bottom-0 right-2 opacity-20 z-0"
+                />
+                <p className="text-gray-400 leading-tight">{review.review}</p>
               </div>
             </div>
           </SwiperSlide>
--- client/src/Pages/AllTrainer/Trainer.jsx
+++ client/src/Pages/AllTrainer/Trainer.jsx
@@ -6,52 +6,47 @@ import { Link } from "react-router-dom";
 
 const Trainer = ({ trainer }) => {
   return (
-    <section>
-      <div className="flex justify-center items-center p-10 ">
-        <div className="bg-white border border-gray-300 rounded-lg shadow-lg overflow-hidden w-full max-w-sm transform hover:scale-105 duration-200 ease-in">
-          <div
-            className="p-4 bg-cover bg-center relative"
-            style={{ backgroundImage: `url(${trainer.photoURL})` }}
-          >
-            <img
-              src={trainer.photoURL}
-              alt={trainer.name}
-              loading="lazy"
-              className="absolute left-1/2 transform -translate-x-1/2 -top-0 rounded-full h-32 w-32 border-4 border-white object-cover"
-            />
+    <section className="flex justify-center items-center p-10 ">
+      <div className="bg-gray-900 border-2 border-gray-500 rounded-lg shadow-lg overflow-hidden w-full max-w-sm transform hover:scale-105 duration-200 ease-in">
+        <div
+          className="p-8 bg-cover bg-center relative"
+          style={{ backgroundImage: `url(${trainer.photoURL})` }}
+        >
+          <img
+            src={trainer.photoURL}
+            alt={trainer.name}
+            loading="lazy"
+            className="absolute left-1/2 transform -translate-x-1/2 -top-0 rounded-full h-32 w-32 border-2 border-gray-500 object-cover"
+          />
+        </div>
+        <div className="p-6 text-center">
+          <h2 className="text-2xl font-semibold text-gray-300 mt-12">
+            {trainer.name}
+          </h2>
+          <h2 className="mt-2 inline-block bg-green-700 text-gray-300 text-sm font-bold px-2 rounded-md">
+            Available
+          </h2>
+          <div className="mt-4">
+            <p className="flex items-center justify-center gap-2 text-gray-400">
+              <MdWorkHistory className="text-orange-800" />
+              <span>{trainer.Experience} years experience</span>
+            </p>
+            <p className="flex items-center justify-center gap-2 text-gray-400">
+              <IoTimeSharp className="text-orange-800" />
+              <span>Available at {trainer.availableTime}</span>
+            </p>
           </div>
-          <div className="p-6 text-center">
-            <h2 className="text-2xl font-semibold text-gray-800 mt-20">
-              {trainer.name}
-            </h2>
-            <h2 className="mt-2 inline-block bg-green-500 text-white text-sm font-bold px-2 rounded-md">
-              Available
-            </h2>
-            <div className="mt-4">
-              <p className="flex items-center justify-center gap-2 text-gray-500">
-                <MdWorkHistory className="text-orange-500" />
-                <span>{trainer.Experience} years experience</span>
-              </p>
-              <p className="flex items-center justify-center gap-2 text-gray-500">
-                <IoTimeSharp className="text-orange-500" />
-                <span>Available at {trainer.availableTime}</span>
-              </p>
-            </div>
-            <div className="flex justify-center mt-4 space-x-4">
-              <FaFacebook className="text-blue-600 cursor-pointer" size={20} />
-              <FaInstagram
-                className="text-orange-500 cursor-pointer"
-                size={20}
-              />
-              <FaLinkedin className="text-blue-700 cursor-pointer" size={20} />
-            </div>
-            <div className="mt-6">
-              <Link to={`/trainers/${trainer._id}`}>
-                <button className="px-4 py-2 bg-orange-500 text-white text-sm font-semibold uppercase rounded-md hover:opacity-85 focus:ring focus:ring-orange-300">
-                  Book Now
-                </button>
-              </Link>
-            </div>
+          <div className="flex justify-center mt-4 space-x-4">
+            <FaFacebook className="text-blue-600 cursor-pointer" size={20} />
+            <FaInstagram className="text-orange-500 cursor-pointer" size={20} />
+            <FaLinkedin className="text-blue-700 cursor-pointer" size={20} />
+          </div>
+          <div className="mt-6">
+            <Link to={`/trainers/${trainer._id}`}>
+              <button className="px-4 py-2 border border-orange-500 text-white text-sm font-semibold uppercase rounded-md hover:opacity-85 focus:ring focus:ring-orange-300">
+                Book Now
+              </button>
+            </Link>
           </div>
         </div>
       </div>
