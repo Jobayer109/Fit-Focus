1a5b53813cf523ccad279aa5c0e825b7eb7f86ad
user.email <jobayer.ahmed109@gmail.com>
2024-06-26 20:16:15 +0600

    Newsletter page done

--- client/src/Components/NewsletterSection/NewsletterSection.jsx
+++ client/src/Components/NewsletterSection/NewsletterSection.jsx
@@ -1,48 +1,58 @@
 import { useForm } from "react-hook-form";
 import { axiosSecure } from "../../Hook/useAxiosSecure";
-import { ToastContainer, toast } from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
-  
-const NewsletterSection = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-   
-    const onSubmit = (data) => {
-      
-        
-        axiosSecure.post("/newsLatter", {
+const NewsletterSection = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    axiosSecure
+      .post(
+        "/newsLatter",
+        {
           ...data,
-          date: new Date().toISOString().split('T')[0]
-      }, {
-            headers: {
-                "Content-Type": 'application/json'
-            }
-        }).then(res=>{
-            
-            if(res.data.message==0){
-                  toast("User already exist");
-            }
-            else{
-                toast("NewsLatter Subscribe successful");
-            }
-        })
-      
-    };
-    return (
-        <div className="mt-20 container mx-auto rounded-xl bg-gradient-to-r from-[#d4d3da] to-white">
-            <div className="lg:grid grid-cols-2 ">
-                <div className="text-center  p-20">
-                    <h1 className="text-2xl font-bold font-Prata">Subscribe Our Newsletter</h1>
-                    <h1>Don't Miss Out! Subscribe to Our Newsletter for the Latest Updates, Exclusive Offers, and Insider Tips. Join Our Community Today and Stay Informed!</h1>
-                </div>
-                <div className="text-center p-20">
-                <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="form-control w-full">
-             
+          date: new Date().toISOString().split("T")[0],
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((res) => {
+        if (res.data.message === 0) {
+          toast("User already exists");
+        } else {
+          toast("Newsletter Subscription successful");
+        }
+      });
+  };
+
+  return (
+    <div className="my-36 container mx-auto rounded-xl bg-gradient-to-l from-black to-orange-600 p-10 shadow-lg">
+      <div className="lg:grid grid-cols-2 gap-8">
+        <div className="text-center p-10">
+          <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-white font-Prata">
+            Subscribe to Our Newsletter
+          </h1>
+          <p className="text-gray-300 mt-4 text-sm md:text-base lg:text-lg">
+            Don't Miss Out! Subscribe to Our Newsletter for the Latest Updates,
+            Exclusive Offers, and Insider Tips. Join Our Community Today and
+            Stay Informed!
+          </p>
+        </div>
+        <div className="text-center p-10">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-control w-full mb-5">
               <input
                 type="text"
                 placeholder="Name"
-                className="input input-bordered  mb-5"
+                className="input input-bordered font-bold w-full"
                 {...register("name", { required: true })}
               />
               {errors.name && (
@@ -50,26 +60,29 @@ const NewsletterSection = () => {
               )}
             </div>
 
-            <div className="form-control w-full">
-             
-              <input 
+            <div className="form-control w-full mb-5">
+              <input
                 type="email"
-                placeholder={ "Email"}
-                className="input input-bordered "
+                placeholder="Email"
+                className="input input-bordered font-bold w-full"
                 {...register("user_email", { required: true })}
               />
-              {errors.email && (
-                <p className="text-red-500 ml-1">Email is required</p>
+              {errors.user_email && (
+                <p className="text-red-500 ml-1">Email is required.</p>
               )}
             </div>
-            <button className="btn text-white bg-[#1E1743] mt-5" type="submit">Subscribe</button>
-        </form>
-                </div>
-                
-            </div>
-            <ToastContainer />
+            <button
+              className="btn text-white bg-[#1E1743] hover:bg-[#120c2f] mt-5 w-full md:w-auto"
+              type="submit"
+            >
+              Subscribe
+            </button>
+          </form>
         </div>
-    );
+      </div>
+      <ToastContainer />
+    </div>
+  );
 };
 
 export default NewsletterSection;
