279144f696e4e40cd04ddbea0278b33c27341521
user.email <jobayer.ahmed109@gmail.com>
2024-06-26 20:18:38 +0600

    Avatar size issue solved

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -52,7 +52,7 @@ const Header = () => {
                     <Avatar
                       img={user.photoURL}
                       rounded
-                      className="w-10 h-10 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500"
+                      className="w-12 h-12 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500"
                     />
                   }
                 >
