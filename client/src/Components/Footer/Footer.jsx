0146e8c07749925c49a61ebacad5f58731fdc720
user.email <jobayer.ahmed109@gmail.com>
2024-06-26 20:57:12 +0600

    Responsive footer

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="px-4  divide-y bg-gray-900 dark:bg-gray-100 text-gray-100">
-      <div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
+      <div className="container flex flex-col items-center justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
         <div className="lg:w-1/3">
           <a
             rel="noopener noreferrer"
@@ -22,10 +22,10 @@ const Footer = () => {
         </div>
         <div className="grid grid-cols-2 text-sm gap-x-3 gap-y-8 lg:w-2/3 sm:grid-cols-4">
           <div className="space-y-3">
-            <h3 className="tracking-wide uppercase text-gray-50 dark:text-gray-900">
+            <h3 className="tracking-wide text-gray-50 dark:text-gray-900">
               Product
             </h3>
-            <ul className="space-y-1">
+            <ul className="space-y-1 text-gray-400">
               <li>
                 <a rel="noopener noreferrer" href="#">
                   Features
@@ -49,10 +49,10 @@ const Footer = () => {
             </ul>
           </div>
           <div className="space-y-3">
-            <h3 className="tracking-wide uppercase text-gray-50 dark:text-gray-900">
+            <h3 className="tracking-wide  text-gray-50 dark:text-gray-900">
               Company
             </h3>
-            <ul className="space-y-1">
+            <ul className="space-y-1 text-gray-400">
               <li>
                 <a rel="noopener noreferrer" href="#">
                   Privacy
@@ -66,10 +66,8 @@ const Footer = () => {
             </ul>
           </div>
           <div className="space-y-3">
-            <h3 className="uppercase text-gray-50 dark:text-gray-900">
-              Developers
-            </h3>
-            <ul className="space-y-1">
+            <h3 className="text-gray-50 dark:text-gray-900">Developers</h3>
+            <ul className="space-y-1 text-gray-400">
               <li>
                 <a rel="noopener noreferrer" href="#">
                   Public API
@@ -88,15 +86,13 @@ const Footer = () => {
             </ul>
           </div>
           <div className="space-y-3">
-            <div className="uppercase text-gray-50 dark:text-gray-900">
-              Social media
-            </div>
+            <div className=" text-gray-50 dark:text-gray-900">Social media</div>
             <div className="flex justify-start space-x-3">
               <a
                 rel="noopener noreferrer"
                 href="#"
                 title="Facebook"
-                className="flex items-center p-1"
+                className="flex items-center p-1 text-blue-600"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -111,7 +107,7 @@ const Footer = () => {
                 rel="noopener noreferrer"
                 href="#"
                 title="Twitter"
-                className="flex items-center p-1"
+                className="flex items-center p-1 text-blue-400"
               >
                 <svg
                   viewBox="0 0 24 24"
@@ -125,7 +121,7 @@ const Footer = () => {
                 rel="noopener noreferrer"
                 href="#"
                 title="Instagram"
-                className="flex items-center p-1"
+                className="flex items-center p-1 text-orange-500"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
