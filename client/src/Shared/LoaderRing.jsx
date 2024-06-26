b64d65733752d2db92018d9730de9aa082052fc1
user.email <jobayer.ahmed109@gmail.com>
2024-06-26 13:25:53 +0600

    Category card transition done

---
+++ client/src/Shared/LoaderRing.jsx
@@ -0,0 +1,5 @@
+export default function Loader() {
+  return (
+    <div className="w-12 h-12 rounded-full animate-spin border-2 border-solid border-primary border-t-transparent m-auto"></div>
+  );
+}
