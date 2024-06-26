8e51f5a3565d0e90693cc7266d0d55e429229d44
user.email <jobayer.ahmed109@gmail.com>
2024-06-26 07:29:02 +0600

    Trainers section

--- client/src/Pages/AllTrainer/AllTrainer.jsx
+++ client/src/Pages/AllTrainer/AllTrainer.jsx
@@ -1,25 +1,18 @@
-
-import Trainer from "./Trainer";
 import useTrainers from "../../Hook/useTrainers";
-
+import Trainer from "./Trainer";
 
 const AllTrainer = () => {
-    const { data, isLoading, error, refetch } =useTrainers()
+  const { data } = useTrainers();
 
-    if (isLoading) {
-        return <div className="bg-red-500 text-3xl text-center">Loading.....</div>
-    }
-    
-    return (
-        <div>
-            
-            <div className="lg:grid grid-cols-3 container mx-auto">
-                {
-                    data?.map(trainer=> <Trainer key={trainer._id} trainer={trainer}></Trainer>)
-                }
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="lg:grid grid-cols-3 container mx-auto">
+        {data?.map((trainer) => (
+          <Trainer key={trainer._id} trainer={trainer}></Trainer>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AllTrainer;
