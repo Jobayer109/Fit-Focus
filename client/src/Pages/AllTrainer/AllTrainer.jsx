0c7f27ae100b4b0cac39ccb5b336c52ce3a06da9
user.email <jobayer.ahmed109@gmail.com>
2024-06-26 16:06:55 +0600

    All trainers section done

--- client/src/Pages/AllTrainer/AllTrainer.jsx
+++ client/src/Pages/AllTrainer/AllTrainer.jsx
@@ -5,7 +5,22 @@ const AllTrainer = () => {
   const { data } = useTrainers();
 
   return (
-    <div className="container mx-auto p-4">
+    <div className="container mx-auto p-4 mt-20">
+      <div className="text-center mb-10">
+        <div className="text-center ">
+          <h1 className="text-4xl font-Prata font-bold text-orange-500">
+            Our All Trainers
+          </h1>
+          <div className="h-2 w-60 mt-1  bg-orange-300 mx-auto blur-lg"></div>
+        </div>
+        <p className="mt-2 text-slate-400">
+          Benefit from custom weight loss plans integrating exercise,
+          <span className="block">
+            diet, and ongoing support, guiding you towards safe and sustainable
+            weight management.
+          </span>
+        </p>
+      </div>
       <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
         {data?.map((trainer) => (
           <Trainer key={trainer._id} trainer={trainer} />
--- client/src/Pages/AllTrainer/Trainer.jsx
+++ client/src/Pages/AllTrainer/Trainer.jsx
@@ -1,74 +1,61 @@
-import { FaCalendar, FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
+/* eslint-disable react/prop-types */
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { IoTimeSharp } from "react-icons/io5";
+import { MdWorkHistory } from "react-icons/md";
 import { Link } from "react-router-dom";
-import { useState } from "react"; 
-
 
 const Trainer = ({ trainer }) => {
-
-  const [showMore, setShowMore] = useState(false);
-  const toggleShowMore = () => {
-    setShowMore(!showMore);
-  };
-
   return (
-    <div>
-      <div>
-        <div className="mt-10 h-[590px] font-Montserrat relative  border border-[#d5dadf] rounded-lg shadow-sm w-[95%]  mx-auto">
-          <div className="">
-            <div className='border-slate-100 bg-[#ffffff] border rounded-xl bg-[#F7F8F8]card card-compact  hover:bg-[#f3f5f3] mb-3 lg:m-0'>
-              <figure>
-                <img className="w-full h-64 rounded-t-2xl " src={trainer.photoURL} alt="Food image" />
-              </figure>
-              <div className="card-body mulish-font text-center rounded-lg">
-                <div>
-                  {trainer?.skills && trainer?.skills.length > 0 ? (
-                    <span className="flex gap-2 items-center justify-center ">
-                      <div className="bg-slate-200 p-1 text- font-bold">
-                        {trainer.skills.join(', ')}
-                      </div>
-                    </span>
-                  ) : (
-                    <span>No skills listed</span>
-                  )}
-                </div>
-                <hr className=' mt-1 h-[1px] border-none bg-slate-200 mx-auto w-[90%] ' />
-                <h2 className="card-title">{trainer.name} <span className="text-base font-medium">({trainer.age} years)</span> </h2>
-                <h2 className=" absolute top-0 left-0 bg-[#E70229] text-white p-2 rounded-2xl">Slot Available</h2>
-                <div className="flex justify-between">
-                  <h1>Experience : {trainer.Experience } years</h1>
-                  <h1>Available Time: {trainer.availableTime}</h1>
-                </div>
-                <p className="text-[#B09696] mb-2 text-left">
-                  {showMore ? trainer.otherInfo : trainer.otherInfo.slice(0, 150)}
-                  <span className="text-[#1882FF] font-bold cursor-pointer ml-1" onClick={toggleShowMore}>
-                    {showMore ? "See Less" : "See More"}
-                  </span>
-                </p>
-                <div className='grid grid-cols-6 gap-0 text-base items-center justify-between text-left'>
-                  
-                 
-                </div>
-                <hr className=' mt-1 h-[1px] border-none bg-slate-200 mx-auto w-[100%] ' />
-                <div className="flex items-center">
-                  
-                  <div className="flex w-full  justify-between items-center">
-                  <div className="flex justify-center items-center  pt-3 space-x-4 align-center">
-                                {/* Social Media Links */}
-                               <FaFacebook size={20}></FaFacebook>
-                               <FaInstagram size={20}></FaInstagram>
-                               <FaLinkedin size={20}></FaLinkedin>
-                            </div>
-                    <div>
-                      <Link to={`/trainers/${trainer._id}`} ><button className="btn px-5 bg-[#12132D] hover:bg-[#434577] text-[#fff]">Know More</button></Link>
-                    </div>
-                  </div>
-                </div>
-              </div>
+    <section>
+      <div className="flex justify-center items-center p-10 ">
+        <div className="bg-white border border-gray-300 rounded-lg shadow-lg overflow-hidden w-full max-w-sm transform hover:scale-105 duration-200 ease-in">
+          <div
+            className="p-4 bg-cover bg-center relative"
+            style={{ backgroundImage: `url(${trainer.photoURL})` }}
+          >
+            <img
+              src={trainer.photoURL}
+              alt={trainer.name}
+              loading="lazy"
+              className="absolute left-1/2 transform -translate-x-1/2 -top-0 rounded-full h-32 w-32 border-4 border-white object-cover"
+            />
+          </div>
+          <div className="p-6 text-center">
+            <h2 className="text-2xl font-semibold text-gray-800 mt-20">
+              {trainer.name}
+            </h2>
+            <h2 className="mt-2 inline-block bg-green-500 text-white text-sm font-bold px-2 rounded-md">
+              Available
+            </h2>
+            <div className="mt-4">
+              <p className="flex items-center justify-center gap-2 text-gray-500">
+                <MdWorkHistory className="text-orange-500" />
+                <span>{trainer.Experience} years experience</span>
+              </p>
+              <p className="flex items-center justify-center gap-2 text-gray-500">
+                <IoTimeSharp className="text-orange-500" />
+                <span>Available at {trainer.availableTime}</span>
+              </p>
+            </div>
+            <div className="flex justify-center mt-4 space-x-4">
+              <FaFacebook className="text-blue-600 cursor-pointer" size={20} />
+              <FaInstagram
+                className="text-orange-500 cursor-pointer"
+                size={20}
+              />
+              <FaLinkedin className="text-blue-700 cursor-pointer" size={20} />
+            </div>
+            <div className="mt-6">
+              <Link to={`/trainers/${trainer._id}`}>
+                <button className="px-4 py-2 bg-orange-500 text-white text-sm font-semibold uppercase rounded-md hover:opacity-85 focus:ring focus:ring-orange-300">
+                  Book Now
+                </button>
+              </Link>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
